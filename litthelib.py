60bb5a34adfe7e6b7341270d8e9987e91ed04b98
Alex Kiura <kiuraalex@gmail.com>
2019-07-15 11:52:36 +0100

    Map commands to args

--- litthelib.py
+++ litthelib.py
@@ -6,6 +6,9 @@ import os
 import re
 import sys
 import zlib
+import logging
+
+logger = logging.Logger(__name__)
 
 argparser = argparse.ArgumentParser(description="The humble version control")
 
@@ -17,21 +20,26 @@ argsubparsers.required = True
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     command = args.command
+
     command_map = {
         "add": cmd_add,
         "cat-file": cmd_cat_file,
-        "checkout": cmd_checkout(args),
-        "commit": cmd_commit(args),
-        "hash-object": cmd_hash_object(args),
-        "init": cmd_init(args),
-        "log": cmd_log(args),
-        "ls-tree": cmd_ls_tree(args),
-        "merge": cmd_merge(args),
-        "rebase": cmd_rebase(args),
-        "rev-parse": cmd_rev_parse(args),
-        "rm": cmd_rm(args),
-        "show-ref": cmd_show_ref(args),
-        "tag": cmd_tag(args)
-
+        "checkout": cmd_checkout,
+        "commit": cmd_commit,
+        "hash-object": cmd_hash_object,
+        "init": cmd_init,
+        "log": cmd_log,
+        "ls-tree": cmd_ls_tree,
+        "merge": cmd_merge,
+        "rebase": cmd_rebase,
+        "rev-parse": cmd_rev_parse,
+        "rm": cmd_rm,
+        "show-ref": cmd_show_ref,
+        "tag": cmd_tag
     }
-    return command_map.get(command)(args)
+    
+    cmd_command = command_map.get(command)
+    if callable(cmd_command):
+        return cmd_command(args)
+    else:
+        logging.error(msg="Command {args} not found".format(args))
