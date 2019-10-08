691ac2af1fad62ed28b74beddeeed5759fb8e323
Alex Kiura <kiuraalex@gmail.com>
2019-10-08 21:46:40 +0300

    WIP Represent Git Repository

--- litthelib.py
+++ litthelib.py
@@ -17,6 +17,50 @@ argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
 
+def repo_file():
+    return
+
+
+def repo_path(repository, *path):
+    """"
+    Compute path under repo's gitdir
+    """
+    return os.path.join(repository.gitdir, *path)
+
+
+def repo_fil(repo, *path):
+    pass
+
+
+
+class GitRepository:
+    """Represents a Git repository"""
+
+    config = None
+    worktree = None
+    gitdir = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git repository {path}")
+
+        # Read config
+        self.config = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.config.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            version = int(self.config.get("core", "repositoryformatversion"))
+            if version != 0:
+                raise Exception(f"Unsupported repositoryformatversion {version}")
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     command = args.command
