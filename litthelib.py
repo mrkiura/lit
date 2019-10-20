7c2fcbc55bc80370b7948a105ed0febfb24cfecb
Alex Kiura <kiuraalex@gmail.com>
2019-10-20 14:19:45 +0300

    Add command to create repo

--- litthelib.py
+++ litthelib.py
@@ -99,7 +99,18 @@ def repo_default_config():
 
     return config
 
+
 argsp = argsubparsers.add_parser('init', help='Initialize a new, empty repository')
+argsp.add_argument('path',
+                   metavar='directory',
+                   nargs='?',
+                   default='.',
+                   help='Where to create the repository.'
+                   )
+
+
+def cmd_init(args):
+    repo_create(args.path)
 
 
 class GitRepository:
