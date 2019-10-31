4031b0d3220915e8079a63d5668542d73e3fe1bd
Alex Kiura <kiuraalex@gmail.com>
2019-10-31 07:10:16 +0300

    Remove dead code

--- litthelib.py
+++ litthelib.py
@@ -147,20 +147,7 @@ def main(argv=sys.argv[1:]):
     command = args.command
 
     command_map = {
-        # 'add': cmd_add,
-        # 'cat-file': cmd_cat_file,
-        # 'checkout': cmd_checkout,
-        # 'commit': cmd_commit,
-        # 'hash-object': cmd_hash_object,
         'init': cmd_init,
-        # 'log': cmd_log,
-        # 'ls-tree': cmd_ls_tree,
-        # 'merge': cmd_merge,
-        # 'rebase': cmd_rebase,
-        # 'rev-parse': cmd_rev_parse,
-        # 'rm': cmd_rm,
-        # 'show-ref': cmd_show_ref,
-        # 'tag': cmd_tag
     }
 
     cmd_command = command_map.get(command)
